14d4c1b9c8307a7e2a100f3471b3e92a494a2718
a2htray <a2htray.yuen@gmail.com>
2021-03-16 18:00:11 +0800

    add a example

--- gra_test.go
+++ gra_test.go
@@ -34,5 +34,24 @@ func Test_ComputeGRGs(t *testing.T) {
 	for _, grg := range grgs {
 		fmt.Println(grg)
 	}
+}
 
+func Test_ComputeGRG2(t *testing.T) {
+	values := [][]float64{
+		{8, 9, 8, 7, 5, 2, 9},
+		{7, 8, 7, 5, 7, 3, 8},
+		{9, 7, 9, 6, 6, 4, 7},
+		{6, 8, 8, 8, 4, 3, 6},
+		{8, 6, 6, 9, 8, 3, 8},
+		{8, 9, 5, 7, 6, 4, 8},
+	}
+	ref := []float64{9, 9, 9, 9, 8, 9, 9}
+
+	rhos := []float64{0.1, 0.3, 0.5, 0.7, 0.9}
+
+	for _, rho := range rhos {
+		Rho = rho
+		fmt.Println("rho", rho)
+		fmt.Println(ComputeGRG(ref, values))
+	}
 }
